c7ca9d4d8395c483dc7248339d6f4e41d4fdd367
Kurt Waechter <kurt.waechter@blackboard.com>
2017-01-31 18:26:44 -0500

    Added logging comments

--- src/test/java/SmokeIT.java
+++ src/test/java/SmokeIT.java
@@ -1,12 +1,14 @@
 import com.kodz.unjenkins.client.JenkinsConsumer;
 import com.kodz.unjenkins.client.dto.JobStats;
 import com.kodz.unjenkins.client.helper.Configuration;
+import com.kodz.unjenkins.logging.Loggable;
 import com.kodz.unjenkins.server.dto.BuildStatus;
 import com.kodz.unjenkins.server.dto.JobStatus;
 import com.kodz.unjenkins.server.dto.Subscription;
 import com.kodz.unjenkins.server.endpoints.websocket.providers.SubscriptionProvider;
 
 import org.awaitility.Duration;
+import org.eclipse.jetty.util.log.Log;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,7 +23,7 @@ import javax.ws.rs.core.Response;
 import static org.awaitility.Awaitility.await;
 
 
-public class SmokeIT {
+public class SmokeIT implements Loggable{
 
     @BeforeClass
     public static void setup() throws Exception {
@@ -48,12 +50,15 @@ public class SmokeIT {
     @Test
     public void isUpdateableTrueTest() throws UnsupportedEncodingException, InterruptedException {
         String jobName = "TestBuilds";
+        logger().info("Getting job status for " + jobName);
         JobStatus jobStatusA = SubscriptionProvider.getJobStatus(jobName);
-        //Trigger new build in Jenkins
+        logger().info("Triggering new build for " + jobName);
         JenkinsConsumer.jenkinsResource.getNewBuild(jobName, URLEncoder.encode("0sec", "UTF-8"));
-
+        logger().info("Waiting for new build to appear in Jenkins...");
         await().atMost(Duration.TWO_SECONDS).until(newBuildIsAdded(jobStatusA));
+        logger().info("Getting updated job status...");
         JobStatus jobStatusB = SubscriptionProvider.getJobStatus(jobName);
+        logger().info("Checking for a true response...");
         assert SubscriptionProvider.updatable(jobStatusA, jobStatusB) : "This should return true, build groups are different.";
     }
 
